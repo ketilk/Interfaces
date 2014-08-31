140290bee261ab8d6de2612f7c708d9aecf8db44
Ketil Karstad <ketil.karstad@gmail.com>
2014-08-31 21:55:23 +0200

    Class name change.

--- ds18b20.py
+++ ds18b20.py
@@ -1,8 +1,12 @@
 import logging
 
-class DS18B20Exception(Exception):
+class DS18B20Error(Exception):
   pass
 
+""" 
+DS18B20 reads data from Dalas temperature probes and returns
+the temperature it reads in Celsius.
+"""
 class DS18B20(object):
     def __init__(self, id):
         self.file_name = '/sys/bus/w1/devices/' + id + '/w1_slave'
@@ -12,15 +16,15 @@ class DS18B20(object):
         try:
             f = open(self.file_name, 'r')
         except IOError:
-            raise DS18B20Exception()
+            raise DS18B20Error()
         try:
             raw = f.read()
         except IOError:
-            raise DS18B20Exception()
+            raise DS18B20Error()
         try:
             f.close()
         except IOError:
-            raise DS18B20Exception()
+            raise DS18B20Error()
         return float(raw.split("t=")[-1])/1000
 
 
